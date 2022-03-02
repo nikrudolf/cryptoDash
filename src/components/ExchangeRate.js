028cf7e3320ca33a89858a35d6f8ebd3278d506b
NiksFuerUngut <61316339+nikrudolf@users.noreply.github.com>
2022-03-02 16:15:58 +0100

    Translate

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -1,7 +1,7 @@
 const ExchangeRate = ({ exchangeRate, chosenPrimaryCurrency, chosenSecondaryCurrency }) => {
   return (
     <div className="exchange-rate">
-      <h3>Exchange Rate</h3>
+      <h3>Wechselkurs</h3>
       <h1>{exchangeRate}</h1>
       <b>{chosenPrimaryCurrency} to {chosenSecondaryCurrency}</b>
 
